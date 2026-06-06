49910ba2b652b035e4c21cbf233865e2e031055e
Abraheem Sofan <EMAIL>
2026-06-06 16:28:30 +0300

    Implement packetized image streaming

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from network.image_receiver import ImageReceiver
+
 import cv2
 import numpy as np
 
 receiver = ImageReceiver(7000)
 
 while True:
-    data = receiver.receive_frame()
+
+    jpeg_bytes = receiver.receive_frame()
 
     image = cv2.imdecode(
-        np.frombuffer(data, dtype=np.uint8),
+        np.frombuffer(
+            jpeg_bytes,
+            dtype=np.uint8
+        ),
         cv2.IMREAD_COLOR
     )
 
     if image is None:
         continue
 
-    cv2.imshow("Camera", image)
+    cv2.imshow(
+        "StrikeX Camera",
+        image
+    )
 
     if cv2.waitKey(1) == 27:
         break
