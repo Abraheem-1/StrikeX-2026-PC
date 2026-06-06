1ae4ed6f93f347ffad5b6cb744416ebafd9c64f0
Abraheem Sofan <EMAIL>
2026-06-06 16:47:34 +0300

    Add dual camera streaming

--- main_camera1.py
+++ main_camera1.py
@@ -1,29 +1,22 @@
 from network.image_receiver import ImageReceiver
-
 import cv2
 import numpy as np
 
-receiver = ImageReceiver(7000)
+receiver = ImageReceiver(7001)
 
 while True:
 
     jpeg_bytes = receiver.receive_frame()
 
     image = cv2.imdecode(
-        np.frombuffer(
-            jpeg_bytes,
-            dtype=np.uint8
-        ),
+        np.frombuffer(jpeg_bytes, dtype=np.uint8),
         cv2.IMREAD_COLOR
     )
 
     if image is None:
         continue
 
-    cv2.imshow(
-        "StrikeX Camera",
-        image
-    )
+    cv2.imshow("Camera1", image)
 
     if cv2.waitKey(1) == 27:
         break
